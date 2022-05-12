4670add368e8d684f5ecc7bbb034281109070d15
suriyarichard <chinnaraj2061971@gmail.com>
2022-05-12 14:59:51 +0530

    search

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -40,6 +40,59 @@ class HomePage extends StatelessWidget {
 
 
 class  MySearchDelegate extends SearchDelegate{
+  List<String> searchResults = [
+  ];
   @override
-  Widget? bulidLeading(BuildContext context) => Container()
+  Widget? buildLeading(BuildContext context) => IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () => close(context, null),
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+    IconButton(
+      icon: Icon(Icons.clear),
+      onPressed: () {
+        if (query.isEmpty){
+          close(context, null);
+        } else{
+          query = '';
+        }
+      },
+    ) 
+  ];
+
+
+  // List<Widget>?buildActions(BuildContext context) => Container();
+  @override
+  Widget buildResults(BuildContext context) => Center(
+    child: Text(
+      query,
+    style: const TextStyle(fontSize: 64),),
+    );
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = searchResults.where((searchResult)
+    {
+      final result = searchResult.toLowerCase();
+      final input = query.toLowerCase();
+      return result.contains(input);
+    }).toList();
+
+    return ListView.builder(
+      itemCount:suggestions.length,
+      itemBuilder:(context, index) {
+        final suggestion = suggestions[index];
+
+        return ListTile(
+          title:Text(suggestion),
+          onTap:(){
+            query = suggestion;
+
+            showResults(context);
+          },
+        );
+      }
+    );
+  }
 }
